27912dc6953bb91888d0a9595c4da6289803e990
Patrizia Petrera <p.petrera@reply.it>
2021-03-06 18:58:04 +0100

    scaffolding Layout and HOC component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Layout>
+          <h1>My Burger Builder</h1>
+        </Layout>
       </div>
     );
   }
