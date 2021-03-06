79f8d0eee20bbd5d5cd1d4f29ede15f467f70177
Patrizia Petrera <p.petrera@reply.it>
2021-03-06 22:11:33 +0100

    Revert "ADD Burger component"
    This reverts commit acf94115e70ac915f22a56b16f9eef546fbe26e2.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,20 +1,11 @@
 import React, { Component } from "react";
-import Burger from "../../components/Burger/Burger";
 import Aux from "../../HOC/Aux";
 
 class BurgerBuilder extends Component {
-  state = {
-    ingredients: {
-      salad: 0,
-      bacon: 0,
-      cheese: 3,
-      meat: 0
-    }
-  }
   render() {
     return (
       <Aux>
-        <Burger ingredients={this.state.ingredients}/>
+        <div>Burger with ingredients</div>
         <div>Burger Controller</div>
       </Aux>
     )
